b99a0d0d9b787fe111c15d057fac2f979ce5a8f3
kotsis <kotsis@users.noreply.github.com>
2018-10-04 00:48:15 +0300

    Initial version of our App

--- src/App.js
+++ src/App.js
@@ -3,23 +3,47 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = { current_color: '000000', selected_color_index: null, color_list: [], error_msg: 'aa' };//katskos: we initiate the applicaiton state here
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  addColorToList(){
+
+  }
+  removeColorFromList(){
+
+  }
+  updateColorInList(){
+
+  }
+
+  handleChange(e) {
+    this.setState({ current_color: e.target.value });
+  }
+
   render() {
+
+    //katskos: conditional div with error message
+    let error_div;
+    if(this.state.error_msg){
+      error_div = <div style={{color: 'red'}}>lala</div>
+    }
+    else{
+      error_div = '';
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          Selected color: {this.state.current_color+1} <br/>
+          Put Hex color here: <input type="text" name="hex_color" value={this.state.current_color} onChange={this.handleChange} /><br/>
+          <button>Add/Update Color</button>
+        </div>
+        {error_div}
+
       </div>
     );
   }
