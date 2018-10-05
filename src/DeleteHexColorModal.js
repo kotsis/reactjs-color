cc9ff6e67acc94a18a708aad31d08391c8e9b344
kotsis <kotsis@users.noreply.github.com>
2018-10-06 01:34:50 +0300

    Updated delete color modal

--- src/DeleteHexColorModal.js
+++ src/DeleteHexColorModal.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import jQuery from 'jquery';
 
 class DeleteHexColorModal extends Component {
   constructor(props){
     super(props);
-    this.id = props.id;
-    //this.state = { current_color: '000000', selected_color_index: null, color_list: [], error_msg: 'aa' };//katskos: we initiate the applicaiton state here
-    //this.handleChange = this.handleChange.bind(this);
+    this._onDelete = this._onDelete.bind(this);
   }
 
   componentDidMount() {
     this.props.onRef(this);
-    jQuery('aaa').html();
   }
   componentWillUnmount() {
     this.props.onRef(undefined);
   }
 
+  _onDelete() {
+    this.props.onDeleteClick(this.props.nid);
+  }
+
   render() {
     return (
-        <div id={this.id}>aaa</div>
+        <div className="modal fade" id={this.props.id} tabIndex="-1" role="dialog" aria-labelledby="exampleModalDeleteHexTitle" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalDeleteHexTitle">Delete Hex color {this.props.nid + 1}</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <div className="row">
+                  <div className="col-md-6 mb-3">
+                    <label htmlFor="hexcolor">HEX color</label>
+                    <input type="text" className="form-control" id="hexcolor" placeholder="" value={'#'+this.props.colorval} readOnly />
+                  </div>
+                  <div className="col-md-2 mb-3">
+                    <label htmlFor="previewcolor">preview</label>
+                    <span className="form-control" id="previewcolor" style={{backgroundColor: '#'+this.props.colorval}}></span>
+                  </div>
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-danger" onClick={this._onDelete}>Delete</button>
+              </div>
+            </div>
+          </div>
+        </div>
     );
   }
 }
